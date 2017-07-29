418c8b1bbc52537d19c43ee56c2148b74180476d
seroj <seroj@serojs-MacBook-Pro-2.local>
2017-07-29 19:19:11 +0400

    home page
    home page + tags

--- app.js
+++ app.js
@@ -2,20 +2,12 @@ var apos = require('apostrophe')({
 	shortName: 'seroj',
 	title: 'Seroj\'s Brain Stash',
 
-	// See lib/modules for basic project-level configuration of our modules
-	// responsible for serving static assets, managing page templates and
-	// configuring user acounts.
-
 	bundles: [ 'apostrophe-blog' ],
 	modules: {
 	    'apostrophe-templates': { viewsFolderFallback: __dirname + '/views' },
-
 		'apostrophe-blog': {},
 		'apostrophe-blog-pages': {},
 		'apostrophe-blog-widgets': {},
-
-		// This configures the apostrophe-assets module to push a 'site.less'
-		// stylesheet
 		'apostrophe-assets': {
 			stylesheets: [
 				{
